525f354e089e4c949e45747851f13d456128c203
Pawel Kreft <pkreft@outlook.de>
2023-01-03 15:16:51 +0100

    Added the master class GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private UIManager uiManager;
+}
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -4,33 +4,19 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class UIManager : MonoBehaviour
 {
-    public static UIManager Instance { get; private set; }
-
+    [SerializeField] private GameManager gameManager;
+    
     [SerializeField] private TextMeshProUGUI stepCounter;
     [SerializeField] private TextMeshProUGUI mistakeCounter;
     [SerializeField] private TextMeshProUGUI hintCounter;
     [SerializeField] private TextMeshProUGUI infoText;
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-            Destroy(this);
-        else
-            Instance = this;
-    }
     
-    // Methods -----
     public void OnClickHintButton()
     {
         //ToDo
     }
     
-    // Getter -----
-    public TextMeshProUGUI GetStepCounter() { return stepCounter; }
-    public TextMeshProUGUI GetMistakeCounter() { return mistakeCounter; }
-    public TextMeshProUGUI GetHintCounter() { return hintCounter; }
-    
-    // Setter -----
+    // ========== Setter ==========
     public void SetStepCounter(int c)
     {
         stepCounter.text = c + "x";
